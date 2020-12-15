db35ed4bdb0b729fd56d5eab8c988b5d2c947d46
stacru <hamaw8888@gmail.com>
2020-12-15 22:40:47 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 'use strict';
 
-// フィボナッチ数列を40番目まで出力する
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 
 // n番目のフィボナッチ数を出力する
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
+  if (memo.has(n)) {
+    return memo.get(n);
   }
-  return fib(n - 1) + fib(n - 2);
+  
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;
 }
 
+// フィボナッチ数列を40番目まで出力する
 const length = 40
 
 for (let i = 0; i <= length; i++) {
