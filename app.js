52e7ff5a7e80f448ac0cdd6df1893e7900d2d2fd
stacru <hamaw8888@gmail.com>
2020-12-15 22:25:26 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// フィボナッチ数列を40番目まで出力する
+
+// n番目のフィボナッチ数を出力する
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+const length = 40
+
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
